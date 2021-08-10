b01be6f6e9eb37522dad56d3739f40afc9bc8a35
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2021-08-10 11:35:15 +0530

    Added Orthodox finger problem

---
+++ orthodox.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void input(vector<int> &ivec){
+  for(auto &i : ivec){
+    cin>>i;
+  }
+}
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> ivec(n);
+  input(ivec);
+  int move= 0;
+  for(int i = 0; i < ivec.size()-1 ; i++){
+    if(ivec[i] != ivec[i+1]){
+      ivec[i] = ivec[i+1];
+      move++;
+    }
+  }
+  cout<<move<<endl;
+   return 0;  
+}
