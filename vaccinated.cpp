3febca789744f5b48947fdf782da5ab56a777c1c
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2021-08-10 11:33:12 +0530

    Added vaccination problem

---
+++ vaccinated.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b , c; 
+    cin>> a >> b >> c;
+    if(a < b && a < c){
+        cout<< "A";
+    }
+    else if(b < c && b < a){
+        cout<< "B";
+        
+    }
+    else {
+        cout << "C";
+    }
+    
+    return 0;
+    
+}
